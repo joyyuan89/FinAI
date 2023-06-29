e1210a8fc7a526ec659ad924d611be7e57fcb971
Jiayue89 <chloeyjy89@gmail.com>
2023-06-29 12:57:33 +0800

    update

---
+++ case_study/bis_crawler/main.sh
@@ -0,0 +1,19 @@
+curl 'https://www.bis.org/review/r230515c.pdf' \
+  -H 'authority: www.bis.org' \
+  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8' \
+  -H 'cache-control: max-age=0' \
+  -H 'cookie: _pk_id.1.6290=d854e26aabbebc68.1675691895.; bisSession=1687686880717; bisUsrID=168768688071769317900; cf_chl_rc_ni=1; cf_chl_2=ccf356817adf28f; cf_clearance=lX05BtKLne0vJn3SNykccAmvGYShc2.cNDhmlCFdDoU-1687694687-0-150; _pk_ref.1.6290=%5B%22%22%2C%22%22%2C1687694697%2C%22https%3A%2F%2Fwww.google.com%2F%22%5D; _pk_ses.1.6290=1; __cf_bm=Kj8n3M9O3Y7ufCdawzujP07hGgjByqwXuf0e5CVNaC4-1687695571-0-Aex3kT9O7vYWICgo4gYaFFSatqJwskkXvHFMnZS2z/SjH7fUW1iahUTu3hbPaxIpTYp8aLDMlmohzWgZqZyE3+M=' \
+  -H 'if-modified-since: Mon, 15 May 2023 04:55:00 GMT' \
+  -H 'if-none-match: W/"252216-1684126500000"' \
+  -H 'referer: https://www.bis.org/review/r230515c.htm' \
+  -H 'sec-ch-ua: "Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "macOS"' \
+  -H 'sec-fetch-dest: document' \
+  -H 'sec-fetch-mode: navigate' \
+  -H 'sec-fetch-site: same-origin' \
+  -H 'sec-fetch-user: ?1' \
+  -H 'upgrade-insecure-requests: 1' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' \
+  --compressed
