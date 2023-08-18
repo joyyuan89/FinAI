f10987f4dd7b393f5b39e92d1afebc66f06d46ec
Jiayue <joyyuan89@gmail.com>
2023-08-18 18:37:38 +0800

    update

--- case_study/central_bank/chatPDF.py
+++ case_study/central_bank/chatPDF.py
@@ -1,21 +1,37 @@
 import streamlit as st
+import tempfile
 import pandas as pd
 
 import os
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 
+
+# config for streamlit app
+st.set_page_config(
+    page_title="ChatPDF",
+    page_icon="📚",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+
 uploaded_file = st.file_uploader("Choose a file")
 #uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 
 if uploaded_file is not None:
 
-    # load and split by page
+    with tempfile.NamedTemporaryFile(delete=False) as tf:
+            tf.write(uploaded_file.getbuffer())
+            file_path = tf.name
 
-    loader = PyPDFLoader(uploaded_file)
-    pages = loader.load()
+            loader = PyPDFLoader(file_path)
+            pages = loader.load()
+    
 
-print(pages[0])
+            text = pages[4].page_content
+
+            st.write(text)
 
 
 
