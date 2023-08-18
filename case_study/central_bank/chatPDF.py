d8dcd4846974854595a07678a6fe64048420325d
Jiayue <joyyuan89@gmail.com>
2023-08-18 18:49:11 +0800

    Update chatPDF.py

--- case_study/central_bank/chatPDF.py
+++ case_study/central_bank/chatPDF.py
@@ -15,6 +15,13 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
+ ## split and add metadata
+text_splitter1 = RecursiveCharacterTextSplitter(
+    # Set a really small chunk size, just to show.
+    chunk_size = 1000,
+    chunk_overlap  = 200,
+    length_function = len,
+)
 
 uploaded_file = st.file_uploader("Choose a file")
 #uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
@@ -27,11 +34,15 @@ if uploaded_file is not None:
 
             loader = PyPDFLoader(file_path)
             pages = loader.load()
-    
 
-            text = pages[4].page_content
+            st.write("load PDF data sucessfully")
 
-            st.write(text)
+
+            ## join all pages into one string
+            number_of_pages = len(pages)
+            full_text = ' '.join([pages[i].page_content for i in range(number_of_pages)])
+
+            texts = text_splitter1.create_documents([full_text])
 
 
 
