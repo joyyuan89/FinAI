909beed1b31aa5f27c248c9b81289ce5ae4030bb
Jiayue89 <chloeyjy89@gmail.com>
2023-08-20 15:32:24 +0800

    update chatPDF

---
+++ case_study/central_bank/chatPDF/app.py
@@ -0,0 +1,95 @@
+import os
+import tempfile
+import streamlit as st
+from streamlit_chat import message
+from pdfquery import PDFQuery
+
+st.set_page_config(
+    page_title="ChatPDF",
+    page_icon="📚",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+def display_messages():
+    st.subheader("💬 Chat")
+    for i, (msg, is_user) in enumerate(st.session_state["messages"]):
+        message(msg, is_user=is_user, key=str(i))
+    st.session_state["thinking_spinner"] = st.empty()
+
+
+def process_input():
+    if st.session_state["user_input"] and len(st.session_state["user_input"].strip()) > 0:
+        user_text = st.session_state["user_input"].strip()
+        with st.session_state["thinking_spinner"], st.spinner(f"Thinking"):
+            query_text = st.session_state["pdfquery"].ask(user_text)
+
+        st.session_state["messages"].append((user_text, True))
+        st.session_state["messages"].append((query_text, False))
+
+
+def read_and_save_file():
+    st.session_state["pdfquery"].forget()  # to reset the knowledge base
+    st.session_state["messages"] = []
+    st.session_state["user_input"] = ""
+
+    for file in st.session_state["file_uploader"]:
+        with tempfile.NamedTemporaryFile(delete=False) as tf:
+            tf.write(file.getbuffer())
+            file_path = tf.name
+
+        with st.session_state["uploading_spinner"], st.spinner(f"uploading {file.name}"):
+            st.session_state["pdfquery"].upload(file_path)
+        os.remove(file_path)
+
+
+def is_openai_api_key_set() -> bool:
+    return len(st.session_state["OPENAI_API_KEY"]) > 0
+
+
+def main():
+    if len(st.session_state) == 0:
+        st.session_state["messages"] = []
+        st.session_state["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY", "")
+        if is_openai_api_key_set():
+            st.session_state["pdfquery"] = PDFQuery(st.session_state["OPENAI_API_KEY"])
+        else:
+            st.session_state["pdfquery"] = None
+
+    st.header("📚 ChatPDF")
+
+    st.markdown("")
+    st.markdown("Key in your OpenAI API key to get started. Skip if you already have it in your environment variables.")
+
+    if st.text_input("OpenAI API Key", value=st.session_state["OPENAI_API_KEY"], key="input_OPENAI_API_KEY", type="password"):
+        if (
+            len(st.session_state["input_OPENAI_API_KEY"]) > 0
+            and st.session_state["input_OPENAI_API_KEY"] != st.session_state["OPENAI_API_KEY"]
+        ):
+            st.session_state["OPENAI_API_KEY"] = st.session_state["input_OPENAI_API_KEY"]
+            if st.session_state["pdfquery"] is not None:
+                st.warning("Please, upload the files again.")
+            st.session_state["messages"] = []
+            st.session_state["user_input"] = ""
+            st.session_state["pdfquery"] = PDFQuery(st.session_state["OPENAI_API_KEY"])
+
+    st.subheader("📄 Upload a document")
+
+    st.file_uploader(
+        "Upload document",
+        type=["pdf"],
+        key="file_uploader",
+        on_change=read_and_save_file,
+        label_visibility="collapsed",
+        accept_multiple_files=True,
+        disabled=not is_openai_api_key_set(),
+    )
+
+    st.session_state["uploading_spinner"] = st.empty()
+
+    display_messages()
+    st.text_input("What's your question?", key="user_input", disabled=not is_openai_api_key_set(), on_change=process_input)
+
+
+if __name__ == "__main__":
+    main()
